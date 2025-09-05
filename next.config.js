d98b223d932c2d2be2f15c81b57aebb82a04318b
wuming818 <149963643+wuming818@users.noreply.github.com>
2025-09-05 20:59:08 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -64,12 +64,11 @@ const nextConfig = {
   },
 };
 
-// PWA 已禁用，因为与 output: 'export' 不兼容
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-//   skipWaiting: true,
-// });
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
