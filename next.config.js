95ae13ad870d987caaa34d2a2c1f3a6af97b6cd1
wuming818 <149963643+wuming818@users.noreply.github.com>
2025-09-05 20:34:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -69,11 +69,11 @@ const nextConfig = {
   },
 };
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
+//const withPWA = require('next-pwa')({
+ // dest: 'public',
+ // disable: process.env.NODE_ENV === 'development',
+ // register: true,
+  //skipWaiting: true,
+//});
 
 module.exports = withPWA(nextConfig);
